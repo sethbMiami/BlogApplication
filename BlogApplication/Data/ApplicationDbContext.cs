b66c3740c5e9cc09debe8189079e1355461759e8
s b <bruner.seth@outlook.com>
2022-10-09 21:12:25 -0400

    Add project files.

---
+++ BlogApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BlogApplication.Models;
+
+namespace BlogApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BlogApplication.Models.Blog> Blog { get; set; }
+    }
+}
---
+++ BlogApplication/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿namespace BlogApplication.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string BlogContent { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+
+        public Blog()
+        {
+
+        }
+    }
+}
